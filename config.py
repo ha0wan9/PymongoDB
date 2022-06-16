ea4a4584f27d9400e26c356095f3eafe734f0663
ha0wan9 <haoran.wang58@gmail.com>
2022-06-16 15:51:44 +0200

    Change project structure

--- config.py
+++
@@ -1,6 +0,0 @@
-# MongoClient('mongodb://username:password@hostnameOrReplicaset/?tls=True') replica by your own Service URI
-URI = "mongodb://haoran:r2SRX3l8KQap7TzJPx6s" \
-      "@node1-581706eeb81da997.database.cloud.ovh.net/" \
-      "admin?replicaSet=replicaset&tls=true"
-
-MAIN_DATABASE = "AI_Backend"
